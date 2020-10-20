ff553306cc0c63c71409e57e98263fbcbcdbae4a
Gabriela Paixão <paixaogabriela00@gmail.com>
2020-10-20 20:23:54 -0300

    Commit 2

--- Customer.java
+++ Customer.java
@@ -5,7 +5,7 @@ public class Customer {
     private String _name;
     private Vector _rentals = new Vector();
 
-    public Customer (String name){
+    public Customer(String name){
         _name = name;
     }
 
@@ -13,8 +13,29 @@ public class Customer {
         _rentals.addElement(arg);
     }
 
-    public String getName (){
+    public String getName() {
         return _name;
+    }
+
+    private double amountFor(Rental each) {
+        double thisAmount = 0;
+        //determine amounts for each line
+        switch (each.getMovie().getPriceCode()) {
+            case Movie.REGULAR:
+                thisAmount += 2;
+                if (each.getDaysRented() > 2)
+                    thisAmount += (each.getDaysRented() - 2) * 1.5;
+                break;
+            case Movie.NEW_RELEASE:
+                thisAmount += each.getDaysRented() * 3;
+                break;
+            case Movie.CHILDRENS:
+                thisAmount += 1.5;
+                if (each.getDaysRented() > 3)
+                    thisAmount += (each.getDaysRented() - 3) * 1.5;
+                break;
+        }
+        return thisAmount;
     }
 
     public String statement() {
@@ -26,22 +47,7 @@ public class Customer {
             double thisAmount = 0;
             Rental each = (Rental) rentals.nextElement();
 
-            //determine amounts for each line
-            switch (each.getMovie().getPriceCode()) {
-                case Movie.REGULAR:
-                    thisAmount += 2;
-                    if (each.getDaysRented() > 2)
-                        thisAmount += (each.getDaysRented() - 2) * 1.5;
-                    break;
-                case Movie.NEW_RELEASE:
-                    thisAmount += each.getDaysRented() * 3;
-                    break;
-                case Movie.CHILDRENS:
-                    thisAmount += 1.5;
-                    if (each.getDaysRented() > 3)
-                        thisAmount += (each.getDaysRented() - 3) * 1.5;
-                    break;
-            }
+            thisAmount = amountFor(each);
 
             // add frequent renter points
             frequentRenterPoints ++;
@@ -61,4 +67,5 @@ public class Customer {
                 " frequent renter points";
         return result;
     }
-} 
+
+}
