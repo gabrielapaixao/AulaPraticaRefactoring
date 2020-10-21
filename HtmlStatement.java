1911a8bc8809331e9df02c6432f5b831bf002184
Gabriela Paixão <paixaogabriela00@gmail.com>
2020-10-20 22:34:58 -0300

    Commit 18

--- HtmlStatement.java
+++ HtmlStatement.java
@@ -1,19 +1,5 @@
 import java.util.Enumeration;
-
-   public class HtmlStatement extends Statement {
-      public String value(Customer aCustomer) {
-      Enumeration rentals = aCustomer.getRentals();
-      String result = valueHead(aCustomer);
-      while (rentals.hasMoreElements()) {
-         Rental each = (Rental) rentals.nextElement();
-         //show figures for each rental
-         result += valueRental(each);
-      }
-      //add footer lines
-      result += valueFooter(aCustomer);
-
-      return result;
-   }
+public class HtmlStatement extends Statement {
 
    public String valueHead(Customer aCustomer) {
    	return "<H1>Rentals for <EM>" + aCustomer.getName() + 
