5c12ee0ce6b5b36b6caffedef80d06133caf919f
Gabriela Paixão <paixaogabriela00@gmail.com>
2020-10-20 21:27:01 -0300

    Commit 9

--- Movie.java
+++ Movie.java
@@ -23,5 +23,27 @@ public class Movie {
     public String getTitle (){
         return _title;
     }
+
+    public double getCharge(int daysRented) {
+	double thisAmount = 0;
+        //determine amounts for each line
+        switch (this.getPriceCode()) {
+            case Movie.REGULAR:
+                thisAmount += 2;
+                if (this.getDaysRented() > 2)
+                    thisAmount += (this.getDaysRented() - 2) * 1.5;
+                break;
+            case Movie.NEW_RELEASE:
+                thisAmount += this.getDaysRented() * 3;
+                break;
+            case Movie.CHILDRENS:
+                thisAmount += 1.5;
+                if (this.getDaysRented() > 3)
+                    thisAmount += (this.getDaysRented() - 3) * 1.5;
+                break;
+        }
+        return thisAmount;
+    }
+
 } 
 
